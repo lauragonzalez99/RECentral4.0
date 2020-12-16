bf76f526a889ea1314b83244595a2845454fce13
shuayb <shuayb.b99@gmail.com>
2020-12-15 20:53:35 -0500

    Manage Customers Screen Reads from Database

--- RECentral4.0/ClientApp/src/app/manage-customers/manage-customers.component.ts
+++ RECentral4.0/ClientApp/src/app/manage-customers/manage-customers.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-manage-customers',
@@ -8,22 +9,33 @@ import { Component, OnInit } from '@angular/core';
 export class ManageCustomersComponent implements OnInit {
 
 
-  customers = [
-    { Name: "John Cena", JoinDate: "2020-09-18", Phone: '416-272-4332', PropertyType: 'Commercial' },
-    { Name: "Alex Moreno", JoinDate: "2020-10-14", Phone: '416-812-4332', PropertyType: 'Commercial' },
-    { Name: "Dan Avadan", JoinDate: "2020-10-17", Phone: '416-412-9332', PropertyType: 'Commercial' },
-    { Name: "Kelly Clarckson", JoinDate: "2020-11-18", Phone: '496-412-4332', PropertyType: 'Commercial' },
-    { Name: "Donald Duck", JoinDate: "2020-12-11", Phone: '436-512-4732', PropertyType: 'Commercial' },
-    { Name: "Donald Duck", JoinDate: "2020-12-11", Phone: '436-512-4732', PropertyType: 'Commercial' },
-    { Name: "Donald Duck", JoinDate: "2020-12-11", Phone: '436-512-4732', PropertyType: 'Commercial' },
-    
+  //customers = [
+  //  { Name: "John Cena", JoinDate: "2020-09-18", Phone: '416-272-4332', PropertyType: 'Commercial' },
+  //  { Name: "Alex Moreno", JoinDate: "2020-10-14", Phone: '416-812-4332', PropertyType: 'Commercial' },
+  //  { Name: "Dan Avadan", JoinDate: "2020-10-17", Phone: '416-412-9332', PropertyType: 'Commercial' },
+  //  { Name: "Kelly Clarckson", JoinDate: "2020-11-18", Phone: '496-412-4332', PropertyType: 'Commercial' },
+  //  { Name: "Donald Duck", JoinDate: "2020-12-11", Phone: '436-512-4732', PropertyType: 'Commercial' },
+  //  { Name: "Donald Duck", JoinDate: "2020-12-11", Phone: '436-512-4732', PropertyType: 'Commercial' },
+  //  { Name: "Donald Duck", JoinDate: "2020-12-11", Phone: '436-512-4732', PropertyType: 'Commercial' },
+  //]
+  public customers: Customers[];
 
-
-  ]
-
-  constructor() { }
+  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+    http.get<Customers[]>(baseUrl + 'customers').subscribe(result => {
+      this.customers = result;
+      console.log(this.customers);
+    }, error => console.error(error));
+  }
 
   ngOnInit(): void {
   }
+
+}
+
+interface Customers {
+  Id: number;
+  Notes: String;
+  Name: String;
+  Email: String;
 
 }
