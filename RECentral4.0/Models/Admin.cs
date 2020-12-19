3b327cb9ecd3adaeeb3f27ffaf79185f007f8993
Laura Gonzalez <laura.a.gonzzalezz@gmail.com>
2020-12-18 21:11:10 -0500

    added admin which is only used for accessing certain prohibited pages and functionalities

---
+++ RECentral4.0/Models/Admin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RECentral4._0.Models
+{
+    public class Admin:Employee
+    {
+        public int AdminID = 0;
+
+        public List<Employee> trainees = new List<Employee>();
+    }
+}
